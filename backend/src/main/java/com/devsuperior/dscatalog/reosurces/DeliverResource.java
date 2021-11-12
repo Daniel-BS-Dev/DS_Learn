979e103d5643eb0d099ff3bfb005efdec2a28da3
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-12 15:03:36 -0300

    Student does not send Revision

--- backend/src/main/java/com/devsuperior/dscatalog/reosurces/DeliverResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/reosurces/DeliverResource.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.reosurces;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +13,13 @@ import com.devsuperior.dscatalog.dto.DeliverRevisionDTO;
 import com.devsuperior.dscatalog.service.DeliverRevisionService;
 
 @RestController
-@RequestMapping(value = "delivers")
+@RequestMapping(value = "/delivers")
 public class DeliverResource {
 
 	@Autowired
 	private DeliverRevisionService service;
 	
+	@PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> saveRevision(@PathVariable Long id, @RequestBody DeliverRevisionDTO dto){
 		service.saveRevision(id, dto);
